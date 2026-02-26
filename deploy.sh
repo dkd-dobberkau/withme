0750dc127d2e40f5ae88a1f77665ff72bd60e274
Olivier Dobberkau <olivier.dobberkau@dkd.de>
2026-02-26 08:12:39 +0100

    Remove deploy.sh from repo, add to .gitignore

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp -P 22 /Users/olivier/Versioncontrol/local/t3-with-me/index.html "olivier.dobberkau@dkd.de@a-udutje"@ssh.isenstedt.project.host:html/t3-with-me/index.html
-echo "Deployed to https://p-e0f3s7.project.space/"
